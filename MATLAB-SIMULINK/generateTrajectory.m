6501874203418cff80b133d5ec3090418ce58ac6
Facundo Cárdenas <facu26giol@gmail.com>
2022-06-19 21:01:18 -0300

    finalización de busqueda de puntos de trayectoria

--- MATLAB-SIMULINK/generateTrajectory.m
+++ MATLAB-SIMULINK/generateTrajectory.m
@@ -1,4 +1,5 @@
 function [] = generateTrajectory(init,weight,to_where)
+
 init = 1;
 weight = 23562;
 to_where = 'to_ship';
@@ -19,7 +20,7 @@ if init
     max_n_cnts = 10; % Cantidad maxima de containers (altura sobre muelle)
     
     % Generamos de forma aleatoria columnas con diferentes alturas.
-    cols_height = generate_cols_height(max_n_cnts,n_cols,c_height);
+    cols_height = generate_cols_height(max_n_cnts,n_cols,c_height)
     
     %Generamos array que contiene los centros de cada columna respecto almuelle
     delta = 0; 
@@ -42,8 +43,9 @@ switch (to_where)
         p1=[-20,0];
         
         % Generamos un numero aleatorio que nos dice a que numero de columna debemos llevar el contenedor. Definimos asi p6.
-        index_goal=randi([1,n_cols],1,1);
-        p6=[cols_centers(index_goal),cols_height(index_goal)];
+        index_goal=randi([1,n_cols],1,1)
+        %index_goal=1;
+        p6=[cols_centers(index_goal),cols_height(index_goal)]
         
         % Generamos un vector que solo contenga las columnas a la izquierda del punto objetivo. Son los unicos puntos que importan.
         left_cols=cols_height(1:index_goal);
@@ -53,14 +55,11 @@ switch (to_where)
         max=0;
         for i=1:index_goal
             if left_cols(i)>=max
-
+                imax_right=i;
                 if left_cols(i)>max
                     imax_left=i;
-                    max=left_cols(i);
-                else
-                    imax_right=i;
-                    max=left_cols(i);
                 end
+            max=left_cols(i);
             end
         end
         
@@ -71,9 +70,11 @@ switch (to_where)
        %% Calculo de p3 y p2
        
        % Definimos la altura de seguridad que me definira la coord y de p3.
+       % Se considera la altura de 2 contenedores para seguridad
        h_safe=max+2*c_height;
        
        % Definimos el x de seguridad, que define la coord. x de p3
+       % Se considera el ancho de un contenedor para seguridad
        x_safe=cols_centers(imax_left)-c_width;
        
        % Primer valor de p3, sin verificacion aun.
@@ -81,7 +82,7 @@ switch (to_where)
        
        % Definimos p2 usando trigonometria.
        p2=[-20, p3(2)-tan(slope)*(p3(1)-p1(1))];
-       
+       plot([p2(1) p3(1)],[p2(2) p3(2)],'r')
        % Bucle que verifica que la trayectoria de p3 a p2 no con otra columna. Si esto ocurre, modificamos el valor de p3
        % y por ende de p2.
        e=0;
@@ -97,7 +98,8 @@ switch (to_where)
            if y23<cols_height(next_index)+2*c_height
                
                 % Si hay colision, entonces debemos actualizar p3 y por
-                % ende, p2.
+                % ende, p2. Para actualizar p3 se corre un ancho a la
+                % izquierda el punto x de p3.
                 e=e+1;
                 p3=[x_safe-e*c_width,h_safe];
                 p2=[-20, p3(2)-tan(slope)*(p3(1)-p1(1))];
@@ -112,16 +114,44 @@ switch (to_where)
        plot(p6(1),p6(2),'bo')
        
        %%  calculo de p4 y p5
-
+       
+       % Si el contenedor tiene que ir a la primer columna, o tiene que ir
+       % a un maximo el p4 y p5 en x seran igual al p6 x. En altura seran
+       %la altura de seguridad
+       if (index_goal ==1 || max==left_cols(index_goal))
+       p4=[p6(1), h_safe];
+       p5=p4;
+              
+       else
+       % Definimos el x de seguridad, que define la coord. x de p3
+       % Se considera el ancho de un contenedor para seguridad
        x_safe=cols_centers(imax_right)+c_width;
+       
+       % Primer valor de p4, sin verificacion aun.
        p4=[x_safe,h_safe];
+       
+       % Definimos p5 usando trigonometria.
        p5=[p6(1), p4(2)+tan(slope)*(p4(1)-p6(1))];
+       hold on
+       plot([p4(1) p5(1)],[p4(2) p5(2)],'r') 
+       % Bucle que verifica que la trayectoria de p4 a p5 no con otra
+       % columna. Si esto ocurre, modificamos el valor de p4
+       % y por ende de p5.
        e=0;
        next_index=imax_right+1;
-       hold on
        while next_index<=index_goal
+           
+           % y45 indica el valor de la recta que une p4 con p5 en el punto
+           % central de cada columna.
            y45=p4(2)+tan(slope)*(p4(1)-cols_centers(next_index));
-           if y45<cols_height(next_index)+2*c_height  
+           
+           % Verificamos que no haya colision entre la recta que une p4
+           % con p5 (mas una distancia de seguridad) y la columna.
+           if y45<cols_height(next_index)+2*c_height
+               
+                % Si hay colision, entonces debemos actualizar p4 y por
+                % ende, p5. Para actualizar p4 se corre un ancho a la
+                % derecha el punto x de p4.
                 e=e+1;
                 p4=[x_safe+e*c_width,h_safe];
                 p5=[p6(1), p4(2)+tan(slope)*(p4(1)-p6(1))];
@@ -130,6 +160,7 @@ switch (to_where)
                 plot([p4(1) p5(1)],[p4(2) p5(2)],'r')
            end
            next_index=next_index+1;
+       end
        end
        plot([p4(1) p5(1)],[p4(2) p5(2)],'r')
        plot([p4(1) p5(1)],[p4(2) p5(2)],'ko')
