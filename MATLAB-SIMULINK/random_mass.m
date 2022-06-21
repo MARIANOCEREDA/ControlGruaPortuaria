376bc2643c6b51c34d1adca7623bbcf9e4d3065e
MARIANOCEREDA <gmarianocereda@gmail.com>
2022-06-21 21:27:20 +0200

    Agregado de comentarios

--- MATLAB-SIMULINK/random_mass.m
+++ MATLAB-SIMULINK/random_mass.m
@@ -1,3 +1,11 @@
-function mass = random_mass(min,max)
-    mass = min+rand(1,1)*(max-min);
+%{
+@ name random_mass
+@ description - Genera una masa aleatoria para agregar al contenedor entre
+los valores minimos y maximos de carga preestablecidos
+@ params {min,max} - carga minima y maxima posibles respectivamente
+@ returns {container_mass} - masa del contenedor (no incluye spreader)
+%}
+
+function container_mass = random_mass(min,max)
+    container_mass = min+rand(1,1)*(max-min);
 end
